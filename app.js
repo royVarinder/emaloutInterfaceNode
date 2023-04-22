3ec8ff624dc80fe88bb20b19ede9d926e7bcc0ed
Varinder Singh <85678326+varinder-speedum@users.noreply.github.com>
2023-04-23 01:06:43 +0530

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const userRouter = require("./api/api_adminUsers/adminUser.Router");
 const userBussRouter = require("./api/api_bussiness/userBussiness.Router");
 const bussCategories = require("./api/api_bussCategories/bussCategories.Router");
-const cors = require("cors");
+// const cors = require("cors");
 
 const storage = multer.diskStorage({
     destination : './upload/images',
@@ -21,7 +21,7 @@ const upload = multer({
 
 app.use(express.json());
 app.use("/profile", express.static('upload/images'));
-app.use(cors());
+// app.use(cors());
 app.use("/api/api_adminUsers", userRouter);
 app.use("/api/api_bussness", userBussRouter);
 app.use("/api/api_bussCategories", bussCategories);
@@ -46,10 +46,10 @@ app.post("/uploadImages", upload.array('profile'), (req, res)=>{
 app.listen(process.env.APP_PORT, ()=>{
     console.log('Your app is running on port:',process.env.APP_PORT);
 });
-app.get("/", (req, res) => {
+// app.get("/", (req, res) => {
     
-    console.log(__dirname)
-    res.sendFile(`${__dirname}/view/index.html`)
-});
+//     console.log(__dirname)
+//     res.sendFile(`${__dirname}/view/index.html`)
+// });
 
 
