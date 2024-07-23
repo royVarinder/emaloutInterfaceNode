ccc1ebce155b4f72869d234c0d8420f98331a794
SKhatakspeedum <102479212+ShalluKhatak@users.noreply.github.com>
2024-07-23 23:39:22 +0530

    Add, update, fetch category by sequilizer.

--- api/api_users/users.Router.js
+++ api/api_users/users.Router.js
@@ -1,12 +1,16 @@
 const {
     getAllNewsListController,
     addUserController,
+    addUpdateCategory,
+    fetchCategory,
 } = require("./users.controller");
 
 const router = require("express").Router();
 const usersCalling = () => {
     try {
-        router.post("/addUser", addUserController)
+        router.post("/addUser", addUserController);
+        router.post("/addUpdateCategory", addUpdateCategory);
+        router.post("/fetchCategory", fetchCategory);
     } catch (error) {
         console.error(error);
     }
--- api/api_users/users.controller.js
+++ api/api_users/users.controller.js
@@ -2,6 +2,8 @@ const {
     getAllNewsListService
 } = require("./users.service")
 const userTable = require("../../models").em_users;
+const categoryTable = require("../../models").em_category;
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
     getAllNewsListController: (req, res) => {
@@ -22,12 +24,89 @@ module.exports = {
     },
     addUserController: async(req, res) => {
         try {
-            console.log('req.body :>> ', req.body);
             const createdUser = await userTable.create(req.body);
-            console.log('createdUser :>> ', createdUser);
 
         } catch (error) {
             console.error(error);
         }
+    },
+    addUpdateCategory: async(req, res) => {
+        try {
+            const { uuid } = req.body;
+             if (!!uuid) {
+                const updatedRow = await categoryTable.update( req.body, {
+                    where: {
+                        uuid,
+                    }
+                });
+                if (updatedRow > 0) {
+                    return res.json(
+                        {
+                            success: 1,
+                            message: "Category updated",
+                        }
+                    )
+                }
+            }else{
+            req.body['uuid'] = uuidv4();
+                const createdRow = await categoryTable.create(req.body);
+                if (!!createdRow) {
+                    return res.json(
+                        {
+                            success: 1,
+                            message: "Category added successfully.",
+                        }
+                    )
+                }
+            }
+            return res.json(
+                {
+                    success: 0,
+                    message: "Something went wrong!",
+                }
+            )
+
+        } catch (error) {
+            console.error(error);
+            return res.json(
+                {
+                    success: 0,
+                    message: error.message,
+                }
+            )
+        }
+    },
+     fetchCategory: async(req, res) => {
+          try {
+        const { uuid } = req.body;
+        
+        if (!!uuid) {
+            const findNews = await categoryTable.findOne({ where: { uuid } });
+            
+            if (findNews) {
+                return res.json({
+                    success: 1,
+                    message: "All category fetched successfully!",
+                    data: findNews,
+                });
+            }
+        }
+        const categoryData = await categoryTable.findAll({ where: { status:1 } });
+                return res.json(
+                    {
+                        success: 1,
+                        message: "All category fetched successfully!",
+                        data: categoryData
+                    }
+                )
+
+
+    } catch (error) {
+        console.error(error);
+        return res.json({
+            success: 0,
+            message: error.message,
+        });
+    }
     }
 }
