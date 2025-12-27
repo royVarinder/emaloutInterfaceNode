99eaa6845eae569f5cf288b132a78fafb1573205
vkmarisetty <63303823+royVarinder@users.noreply.github.com>
2025-12-27 21:31:29 +0530

    Update router.js

--- api/organizations/router.js
+++ api/organizations/router.js
@@ -9,7 +9,7 @@ router.post("/create-organization", verifyToken, uploadFiles.array('logo'), orga
 router.post("/update-org", verifyToken, organizationController.updateOrganization);
 router.post("/get-org", verifyToken, organizationController.getOrganization);
 router.post("/update-org-logo", verifyToken, uploadFiles.array('files'), organizationController.updateOrgLogo);
-router.post("/create-event", verifyToken, uploadFiles.array('event_image'), organizationController.addOrgEvent);
+router.post("/create-event", uploadFiles.array('event_image'), organizationController.addOrgEvent);
 router.post("/update-event", verifyToken, uploadFiles.array('event_image'), organizationController.updateOrgEvent);
 router.post("/get-event", verifyToken, organizationController.getOrgEvent);
 
